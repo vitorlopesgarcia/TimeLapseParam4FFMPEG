ea1433d72692548745d80a1f90a6ec76d3109d64
vitorlopesgarcia <vitorlopesgarcia@users.noreply.github.com>
2018-07-21 19:58:47 +1200

    # Reducing video speed

--- convert.sh
+++ convert.sh
@@ -6,12 +6,14 @@ ffmpeg -threads 4 -i Image%03d.JPG -vf fps=25 -f avi -c:v rawvideo  movie.avi
 # From AVI to mp4 (compatible with Instagram)
 ffmpeg -i movie.avi -vf scale=-1:1080 -c:v libx264 -crf 18  -preset veryslow -tune film -profile:v baseline -level 3.0 -pix_fmt yuv420p  movie.mp4
 
-# Single pass to mp4 (is it compatible with Instagram?)
+# Single pass to mp4 (compatible with Instagram?)
 ffmpeg -threads 4 -i DSC_%04d.JPG -vf scale=-1:1080 -vf fps=50 -c:v libx264 -crf 18 -preset veryslow -tune film -profile:v baseline -level 3.0 -pix_fmt yuv420p movie.mp4
 
 # Single pass to mp4, using Nvidia encoder (Compatible with Instagram, maximum width for input=4000)
 ffmpeg -hwaccel cuvid -i DSC_%04d.JPG -vf scale=-1:720 -vf fps=40 -c:v h264_nvenc  -preset slow -profile:v high -b:v 15M  -pix_fmt yuv420p -vf deshake  movie.mp4
 
+# Reducing video speed
+ffmpeg -i movie.mp4 -r 30 -filter:v "setpts=0.25*PTS" output.mp4
 
 # Other Parameters
 -vf "hflip,vflip" # Flips
